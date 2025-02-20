0eb298053fb72887326ff1f78a45898969027e13
Andrew Noyes <noyesa@gmail.com>
2025-02-19 16:37:15 -0800

    Implemented forecast fetching

--- src/fetch-forecast.test.mjs
+++ src/fetch-forecast.test.mjs
@@ -1,8 +1,45 @@
-import { describe, it, expect } from 'vitest';
-import { fetchDiscussion } from './fetch-forecast.mjs';
+import { describe, it, expect, beforeAll, afterEach, afterAll } from 'vitest';
+import { FetchMocker } from 'mentoss';
+
+import { fetchDiscussion, createDiscussionURL } from './fetch-forecast.mjs';
+import { getMockServer } from '../test/mock-server.mjs';
 
 describe('fetchDiscussion', () => {
-  it('exists', () => {
-    expect(fetchDiscussion).toBeTypeOf('function');
+  let mocker;
+
+  beforeAll(() => {
+    mocker = new FetchMocker({
+      servers: [getMockServer()],
+    });
+
+    mocker.mockGlobal();
+  });
+
+  afterEach(() => {
+    mocker.clearAll();
+  });
+
+  afterAll(() => {
+    mocker.unmockGlobal();
+  });
+
+  it('fetches data for the forecast office', async () => {
+    const response = await fetchDiscussion('mtr');
+    expect(response.status).toBe(200);
+  });
+});
+
+describe('createDiscussionURL', () => {
+  it('returns a URL pointing to the NWS', () => {
+    const url = createDiscussionURL('mtr');
+    expect(url).toMatch(/^https:\/\/forecast.weather.gov\/product.php/);
+
+    const { searchParams } = new URL(url);
+
+    expect(searchParams.get('site')).toMatch(/mtr/i);
+    expect(searchParams.get('issuedby')).toMatch(/mtr/i);
+    expect(searchParams.get('product')).toMatch(/afd/i);
+    expect(searchParams.get('format')).toMatch(/txt/i);
+    expect(searchParams.get('glossary')).toBe('0');
   });
 });
---
+++ test/mock-server.mjs
@@ -0,0 +1,42 @@
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+
+import { MockServer } from 'mentoss';
+
+/**
+ * Sets up a server with the NWS URL.
+ */
+const getMockNWSServer = () => new MockServer('https://forecast.weather.gov');
+
+/**
+ * Loads the mock response from file on disk.
+ * @returns {string} The contents of the mock response.
+ */
+const loadMockResponse = () =>
+  readFileSync(join(import.meta.dirname, 'example-discussion.html'), {
+    encoding: 'utf8',
+  });
+
+/**
+ * Sets the server to respond with a mock response.
+ * @param {MockServer} server - The mock server instance.
+ */
+const setMockResponse = (server) => {
+  server.get('/product.php', {
+    status: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: loadMockResponse(),
+  });
+};
+
+/**
+ * Get the mock server for the NWS forecast discussion page.
+ * @returns Mock server.
+ */
+export const getMockServer = () => {
+  const server = getMockNWSServer();
+  setMockResponse(server);
+  return server;
+};
